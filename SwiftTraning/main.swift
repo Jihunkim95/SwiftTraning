f7ddeee2748535274d49c76d440f94f842ca9655
Jihunkim95 <darak551@naver.com>
2023-10-10 20:13:09 +0900

    튜플사용법 학습

--- SwiftTraning/main.swift
+++ SwiftTraning/main.swift
@@ -6,61 +6,19 @@
 //
 
 import Foundation
-//
-////let fib = { (_ n) in  n <= 2 ? 1 : fib(n-1) + fib(n-2)
-////}
-//
-////1. 중앙값 찾기
-////2. 구분해서 좌우측 나누기
-////3. 탈출조건 : 전 중앙값 < 중앙값 < 후 중앙값
-////   첫값 < 중앙값 < 마지막값
-//let quicksort = { (_ n:[Int]) in
-//    var L:[Int] = []
-//    var R:[Int] = []
-//    let mIdx:Int = n.count/2
-//    
-//    //탈출조건 
-//    //1.전 중앙값 < 중앙값 < 후 중앙값
-//    // 첫값 < 중앙값 < 마지막값
-////    print(n)
-//    var flag:Bool = true
-//    
-//    for i in 0...mIdx{
-//        if n[i] > n[mIdx]{
-//            flag = false
-//        }
-//    }
-//    for i in mIdx..<n.count{
-//        if n[i] < n[mIdx]{
-//            flag = false
-//        }
-//    }
-//    
-////    if n[mIdx] < n[mIdx+1] && n[mIdx-1] < n[mIdx] &&
-////       n[0] < n[mIdx] && n[n.count-1] > n[mIdx]{
-////    print(n.forEach{$0 < n[mIdx]})
-//    print(n,n[mIdx] )
-//        if flag{
-//        return n
-//    }else{
-//        //구분하여 좌 우 나누기
-//        for i in n.indices{
-//            n[i] <= n[mIdx] ? L.append(n[i]) : R.append(n[i])
-//        }
-//        return quicksort(L+R)
-//    }
-//    
-//}
-//
-//print(quicksort([50,11,5,1,2,3,20]))
 
-func quicksort(_ list: [Int]) -> [Int] {
-    if list.count <= 1 { return list }
-    let pivot: Int = list.first!
-    var left: [Int] = []
-    var right: [Int] = []
-//    print(list)
-    list[1...].forEach { $0 < pivot ? left.append($0) : right.append($0) }
-    return quicksort(left) + [pivot] + quicksort(right)
-}
-print(quicksort([50,11,5,1,2,3,20]))
+/*
+예제 04
+수 하나를 입력받아서 일의 자리를 1로 바꾸는 앱을 만들어 보세요. (만일 변수 num의 값이 456 이라면 451이 되고, 777이라면 771이 된다)
+ */
+print("입력",terminator: " ")
+
+var num:Int = Int(readLine()!)!
+var result:Int = 0
+
+num/10 == 0 ? print("결과 \(num)") : print("결과 \(num/10*10+1)")
+
+/*
+예제 05
+수 하나를 입력받아서 일의 자리를 1로 바꾸는 앱을 만들어 보세요. (만일 변수 num의 값이 456 이라면 451이 되고, 777이라면 771이 된다)
+ */
